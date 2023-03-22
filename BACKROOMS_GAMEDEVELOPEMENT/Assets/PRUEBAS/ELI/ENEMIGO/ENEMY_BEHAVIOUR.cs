3a2eccba61007c8a835ee3a6ca23cef4674d3741
Klizzza <EscolarKenia@gmail.com>
2023-03-21 18:46:24 -0600

    ELIZABETH_PRUEBAS
    Solo hice algunas pruebas de luces y otras cositas :-)

---
+++ BACKROOMS_GAMEDEVELOPEMENT/Assets/PRUEBAS/ELI/ENEMIGO/ENEMY_BEHAVIOUR.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMY_BEHAVIOUR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* TUTORIALES
+     * Comportamiento del enemigo:
+     * https://www.youtube.com/watch?v=_5pxcUykXcA
+     * Modelo y animaciones desde Mixamo:
+     * https://www.youtube.com/watch?v=PesrC6HcYB8
+    */
+
+}

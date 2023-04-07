3d8fb1ae47fff0dd8867c54d953b7b25dbb6b23e
Klizzza <EscolarKenia@gmail.com>
2023-04-07 17:51:18 -0600

    Trabajando en las mecánicas de la linterna :)

--- BACKROOMS_GAMEDEVELOPEMENT/Assets/FORMAL_BACKROOMS_GAME/SCRIPTS/Lampara/On_Off.cs
+++ BACKROOMS_GAMEDEVELOPEMENT/Assets/FORMAL_BACKROOMS_GAME/SCRIPTS/Lampara/On_Off.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class On_Off : MonoBehaviour
 {
     [SerializeField] Light flashlight;
+    [SerializeField] TMP_Text no_bateria_txt;
+    [SerializeField] TMP_Text bateria_restante_txt;
     private bool encendido;
+    private float bateria_restante = 5;
+    private float no_baterias = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +22,10 @@ public class On_Off : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Textos();
         Onoff();
+        Bateria_Restante();
+        Baterias();
     }
     void Onoff()
     {
@@ -36,6 +45,49 @@ public class On_Off : MonoBehaviour
                 encendido = false;
                 return;
             }
+        }
+    }
+
+    void Textos()
+    {
+        no_bateria_txt.text = "Baterias: " + no_baterias.ToString();
+        bateria_restante_txt.text = "Bateria restante: " + bateria_restante.ToString() + "%";
+    }
+
+    void Bateria_Restante()
+    {
+        if (encendido == true)
+        {
+            bateria_restante -= 1 * Time.deltaTime;
+        }
+        if (bateria_restante <= 0)
+        {
+            flashlight.intensity = 0;
+            encendido = false;
+            bateria_restante = 0;
+        }
+        if (bateria_restante >= 10)
+        {
+            bateria_restante = 10;
+        }
+    }
+
+    void Baterias()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && no_baterias >= 1)
+        {
+            no_baterias -= 1;
+            bateria_restante += 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && no_baterias == 0)
+        {
+            return;
+        }
+
+        if (no_baterias <= 0)
+        {
+            no_baterias = 0;
         }
     }
 }
